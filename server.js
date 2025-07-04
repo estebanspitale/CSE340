7e75215aed049ba852abccfcf7929ce83ba99672
Esteban Spitale <estebanspitale089@gmail.com>
2025-07-04 06:15:17 -0300

    Express Routes

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
 
 
 /* ***********************
