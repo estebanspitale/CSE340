11b31cb756e742169ead27641e2a57775cf80afe
Esteban Spitale <estebanspitale089@gmail.com>
2025-07-04 06:37:45 -0300

    corection

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-// Index route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home" });
-});
-
 
 /* ***********************
  * View Engine and Templates
@@ -28,6 +23,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
